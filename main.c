ec8db97ca969ec0735e689f2ab51e022079788dd
rachael_ashby <rachael.ashby@gmail.com>
2016-01-29 22:49:54 +1300

    Mac build

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <time.h>
 #include "SDL.h"
-#include "SDL2/SDL_image.h"
-#include "SDL2/SDL_mixer.h"
+#include "SDL2_Image/SDL_image.h"
+#include "SDL2_Mixer/SDL_mixer.h"
 #include <stdbool.h>
 #include <assert.h>
 #include "common.h"
