a6c77c04be4e6929f4dd22bb9b4250850e852681
Les Miskin <trilinear.nz@gmail.com>
2016-01-30 13:43:45 +1300

    Minor fixes to bobbing and player positioning

--- scene.c
+++ scene.c
@@ -14,30 +14,40 @@ void sceneAnimateFrame() {
         bobDir = true;
     }
 
-    if(bob2Dir && bob2Inc > 1) {
+    if(bob2Dir && bob2Inc > 2) {
         bob2Dir = false;
-    }else if(!bob2Dir && bob2Inc < 0){
+    }else if(!bob2Dir && bob2Inc < 1){
         bob2Dir = true;
     }
 
     if(bobDir) {
         bobInc += 0.5;
-        bob2Inc -= 0.25;
     }else{
         bobInc -= 0.5;
-        bob2Inc += 0.25;
+    }
+
+    if(bob2Dir) {
+        bob2Inc += 0.5;
+    }else{
+        bob2Inc -= 0.5;
     }
 }
 
 void sceneRenderFrame() {
+    int xOffset = 3;
+    int yOffset = 30;
+    
     Sprite sprite = makeSimpleSprite("office-bg.png");
     drawSprite(sprite, makeCoord(screenBounds.x/2, screenBounds.y/2));
     Sprite chair1 = makeSimpleSprite("chair-bottom.png");
-    drawSprite(chair1, makeCoord(screenBounds.x/2, screenBounds.y/2));
+    drawSprite(chair1, makeCoord(screenBounds.x/2 - xOffset, screenBounds.y/2 + yOffset));
     Sprite body = makeSimpleSprite("body-yellow.png");
-    drawSprite(body, makeCoord(screenBounds.x/2, screenBounds.y/2 - bob2Inc));
+    drawSprite(body, makeCoord(screenBounds.x/2 - xOffset, screenBounds.y/2 + yOffset - bob2Inc));
     Sprite chair2 = makeSimpleSprite("chair-top.png");
-    drawSprite(chair2, makeCoord(screenBounds.x/2, screenBounds.y/2));
+    drawSprite(chair2, makeCoord(screenBounds.x/2 - xOffset, screenBounds.y/2 + yOffset));
     Sprite head = makeSimpleSprite("head-black.png");
-    drawSprite(head, makeCoord(screenBounds.x/2, screenBounds.y/2 + 1 - bobInc));
+    drawSprite(head, makeCoord(screenBounds.x/2 - xOffset, screenBounds.y/2 + yOffset + 1 - bobInc));
+
+    Sprite coffee = makeSimpleSprite("coffee.png");
+    drawSprite(coffee, makeCoord(235, screenBounds.y/2 + 5));
 }
